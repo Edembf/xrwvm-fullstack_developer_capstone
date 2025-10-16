7b59aea85d0d089247436a518651acf546c3f4d1
Edembf <savadogo5@live.com>
2025-10-16 17:37:54 -0400

    module 04 accomplish

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -153,10 +153,9 @@ def get_dealer_reviews(request, dealer_id):
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
 @csrf_exempt
-
 def get_dealer_details(request, dealer_id):
-    if dealer_id:
-        endpoint = f"/fetchDealer/{dealer_id}"
+    if (dealer_id):
+        endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
